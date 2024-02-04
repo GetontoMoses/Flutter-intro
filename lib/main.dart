8fefd2e5b377783d89d78fb37e3c682d51885634
Getonto3 <getontomoses18@gmail.com>
2024-02-04 18:15:28 +0300

    feat: implementing customFfield widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/configurations/constants.dart';
+import 'package:flutter_application_1/views/customtext.dart';
+import 'package:flutter_application_1/views/customField.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Home(),debugShowCheckedModeBanner: false,
-  )); //MaterialApp
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class Home extends StatelessWidget {
   const Home({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
+    final TextEditingController PasswordContoller = TextEditingController();
+    final TextEditingController UsernameContoller = TextEditingController();
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
-        backgroundColor:primary,
-        foregroundColor: secondary,
+        title: const CustomText(
+          label: "DU App",
+          labelColor: appWhiteColor,
+        ),
+        backgroundColor: primaryColor,
+        foregroundColor: appWhiteColor,
       ),
-      body: Text("First App"),
+      body:
+          const Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        CustomText(
+            label: "Login Screen", fontSize: 30, fontWeight: FontWeight.bold),
+        CustomText(
+          label: "Username",
+         // CustomTextField(controller: UsernameContoller,),
+          labelColor: primaryColor,
+        ),
+        CustomText(label: "Password"),
+       // CustomTextField(controller: PasswordContoller,)
+      ]),
     );
   }
 }
+
