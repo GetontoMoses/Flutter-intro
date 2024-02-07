818642716d7753628a4f62467fc7270e7fffda21
Getonto3 <getontomoses18@gmail.com>
2024-02-07 16:29:35 +0300

    feat: impelementing custom buttom

--- lib/main.dart
+++ lib/main.dart
@@ -28,45 +28,54 @@ class Home extends StatelessWidget {
         backgroundColor: primaryColor,
         foregroundColor: appWhiteColor,
       ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Center(
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: CustomText(
-                  label: "Login Screen",
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/Daystarlogo.png",
+                height: 120.0,
+                width: 120.0,
+              ),
+              const Center(
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: CustomText(
+                    label: "Login Screen",
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: CustomText(
-                label: "Username",
-                labelColor: Colors.blue,
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CustomText(
+                  label: "Username",
+                  labelColor: Colors.blue,
+                ),
               ),
-            ),
-            CustomTextField(
-                customTextFieldController: usernameController,
-                hintText: "Enter your username",
-                prefixIcon: const Icon(Icons.person)),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: CustomText(label: "Password"),
-            ),
-            CustomTextField(
-              customTextFieldController: passwordController,
-              hintText: "Enter your password",
-              prefixIcon: const Icon(Icons.lock),
-              suffixIcon: const Icon(Icons.visibility),
-            ),
-            // const CustomTextButton(
-            //   buttonName: "Log In",
-            // )
-          ]),
+              CustomTextField(
+                  Controller: usernameController,
+                  hintText: "Enter your username",
+                  prefixIcon: const Icon(Icons.person)),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CustomText(label: "Password"),
+              ),
+              CustomTextField(
+                Controller: passwordController,
+                hintText: "Enter your password",
+                prefixIcon: const Icon(Icons.lock),
+                suffixIcon: const Icon(Icons.visibility),
+              ),
+              const SizedBox(height: 20),
+              // const CustomTextButton(
+              //   buttonName: "Log In",
+              // )
+            ]),
+            
+      ),
     );
   }
 }
