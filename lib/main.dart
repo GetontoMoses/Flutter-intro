62b872111d8882cf93bdf860fefcce899ee9465f
Getonto3 <getontomoses18@gmail.com>
2024-02-21 21:25:55 +0300

    feat: implementing routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/route.dart';
 import 'package:flutter_application_1/views/login.dart';
 import 'package:flutter_application_1/views/calculator.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Calculator(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(GetMaterialApp(
+      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
+      getPages: Routes.routes));
 }
